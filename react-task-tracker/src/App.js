7b73eedcb4c5a70e5415dfd5c66649dc1ddae202
Sarah Gerrard <98355961+LadyBluenotes@users.noreply.github.com>
2022-06-29 22:37:57 -0700

    starting of tutorial

---
+++ react-task-tracker/src/App.js
@@ -0,0 +1,16 @@
+import Header from './Components/Header'
+// import Tasks from './Components/Tasks'
+
+function App() {
+  return (
+    <div className="container">
+      <Header />
+      {/* <Tasks /> */}
+    </div>
+  );
+}
+
+export default App;
+
+
+// at 43 minutes for traversy video on react
---
+++ react-task-tracker/src/Components/Tasks.js
@@ -0,0 +1,17 @@
+// import { useState } from 'react'
+
+
+
+// const Tasks = () => {
+//     const [tasks, setTasks] = useState([]);
+
+//     return (
+//         <>
+//             {tasks.map((task) => (
+//                 <h3>
+//                     {task.id} {task.title}
+//                 </h3>
+//             ))}
+//         </>
+//     )
+// }
